f089caf90b27ad2002fc54322574a9da019b1ff6
Kyllder Rocha <krocha@ness.com.br>
2022-12-08 16:07:27 -0300

    Adicionando busca da cidade no problema

--- src/problema/problema.module.ts
+++ src/problema/problema.module.ts
@@ -3,9 +3,10 @@ import { ProblemaService } from './problema.service';
 import { ProblemaController } from './problema.controller';
 import { PrismaService } from 'src/database/PrismaService';
 import { FirebaseService } from 'src/firebase/firebase.service';
+import { CidadeService } from 'src/cidade/cidade.service';
 
 @Module({
   controllers: [ProblemaController],
-  providers: [ProblemaService, PrismaService, FirebaseService]
+  providers: [ProblemaService, CidadeService, PrismaService, FirebaseService]
 })
 export class ProblemaModule {}
